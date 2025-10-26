b175f90ed8caeb4b630f0f759c41de507ab1cf0e
asimon84 <ajsimon4@yahoo.com>
2025-10-26 16:49:13 -0700

    layout updates

---
+++ resources/views/partials/main-menu.blade.php
@@ -0,0 +1,158 @@
+<header class="quanto-header main-header bg-color-white" id="sticky-menu">
+    <div class="sticky-wrap">
+        <div class="sticky-active">
+            <div class="container custom-container">
+                <div class="row gx-3 align-items-center justify-content-between">
+                    <div class="col-8 col-sm-auto">
+                        <div class="header-logo">
+                            <a href="{{ route('home') }}">
+                                <img src="{{ asset('/storage/images/logo-1.svg') }}" alt="logo" />
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col text-end text-lg-center">
+                        <nav class="main-menu menu-style1 d-none d-lg-block">
+                            <ul>
+                                <li class="menu-item-has-children">
+                                    <a href="#">Home</a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="{{ route('home') }}">Overview</a>
+                                        </li>
+                                        <li>
+                                            <a href="index-2.html">Creative Agency</a>
+                                        </li>
+                                        <li>
+                                            <a href="index-3.html">Design Studio</a>
+                                        </li>
+                                        <li>
+                                            <a href="index-4.html">branding agency</a>
+                                        </li>
+                                        <li>
+                                            <a href="index-5.html">modern agency</a>
+                                        </li>
+                                        <li>
+                                            <a href="index-6.html">personal portfolio</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#">Pages</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="about.html">About Us</a></li>
+                                        <li class="menu-item-has-children">
+                                            <a class="no-border" href="#">Service</a>
+                                            <ul class="sub-menu">
+                                                <li><a href="service.html">Service - 1</a></li>
+                                                <li><a href="service-2.html">Service - 2</a></li>
+                                                <li>
+                                                    <a href="service-details.html"
+                                                    >Service Details</a
+                                                    >
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li class="menu-item-has-children">
+                                            <a class="no-border" href="#">Career</a>
+                                            <ul class="sub-menu">
+                                                <li><a href="career.html">Career</a></li>
+                                                <li>
+                                                    <a href="career-details.html">Career Details</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li class="menu-item-has-children">
+                                            <a class="no-border" href="#">Team</a>
+                                            <ul class="sub-menu">
+                                                <li><a href="team.html">Team</a></li>
+                                                <li>
+                                                    <a href="team-details.html">Team Details</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <a href="pricing.html">Pricing</a>
+                                        </li>
+                                        <li>
+                                            <a href="faq.html">FAQ's</a>
+                                        </li>
+                                        <li>
+                                            <a href="404.html">404</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#">Portfolio</a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="portfolio-masonry.html"
+                                            >Portfolio Masonry</a
+                                            >
+                                        </li>
+                                        <li>
+                                            <a href="portfolio-standard.html"
+                                            >Portfolio Standard</a
+                                            >
+                                        </li>
+                                        <li>
+                                            <a href="portfolio-gallery.html"
+                                            >Portfolio Gallery</a
+                                            >
+                                        </li>
+                                        <li>
+                                            <a href="portfolio-slider.html">Portfolio Slider</a>
+                                        </li>
+                                        <li>
+                                            <a href="portfolio-card.html">Portfolio Card</a>
+                                        </li>
+                                        <li>
+                                            <a href="portfolio-details.html"
+                                            >Portfolio Details</a
+                                            >
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#">Blog</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="blog-grid.html">Blog Grid</a></li>
+                                        <li><a href="blog-list.html">Blog List</a></li>
+                                        <li><a href="blog-details.html">Blog Details</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="contact.html">Contact</a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <button
+                                class="menuBar-toggle quanto-menu-toggle d-inline-block d-lg-none"
+                        >
+                            <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="40"
+                                    height="40"
+                                    viewBox="0 0 40 40"
+                                    fill="none"
+                            >
+                                <path
+                                        d="M24.4444 26V28H0V26H24.4444ZM40 19V21H0V19H40ZM40 12V14H15.5556V12H40Z"
+                                        fill="currentColor"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="col-auto d-none d-lg-block">
+                        <a class="quanto-link-btn btn-pill" href="./contact.html"
+                        >Get in touch
+                            <span>
+                      <i class="fa-solid fa-arrow-right arry1"></i>
+                      <i class="fa-solid fa-arrow-right arry2"></i>
+                    </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
