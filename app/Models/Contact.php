be75582726b8a19a790bc8b8ddd461ce25ab0106
asimon84 <ajsimon4@yahoo.com>
2025-10-28 20:50:12 -0700

    contact model update

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,9 +2,60 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    //
+    /** @use HasFactory<\Database\Factories\ContactFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'company_name',
+        'email',
+        'subject',
+        'message',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'name' => 'string',
+            'company_name' => 'string',
+            'email' => 'string',
+            'subject' => 'string',
+            'message' => 'string'
+        ];
+    }
+
+    /**
+     * Send an email from a contact object
+     *
+     * @param int $id
+     *
+     * @return bool
+     */
+    function sendemail(int  $id): bool {
+
+    }
 }
