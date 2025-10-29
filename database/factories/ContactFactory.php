1bbbb701ebd4cb2bda11503be4d4413f5d65cd48
asimon84 <ajsimon4@yahoo.com>
2025-10-28 20:53:33 -0700

    contact factory added

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'company_name' => fake()->name(),
+            'email' => fake()->->safeEmail(),
+            'subject' => Str::random(10),
+            'message' => Str::random(255),
+        ];
+    }
+}
