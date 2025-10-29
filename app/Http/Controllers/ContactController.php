fe43f44e7f38f0d2e7bce66a9375dd117d79d75f
asimon84 <ajsimon4@yahoo.com>
2025-10-28 20:37:54 -0700

    contact method and class

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -16,5 +17,26 @@ class ContactController extends Controller
      */
     public function index(Request $request):View {
         return view('contact', []);
+    }
+
+    /**
+     * Process contact form submission, return success or failure
+     *
+     * @param Request $request
+     *
+     * @return bool
+     */
+    public function form(Request $request):bool {
+        $contact = new Contact();
+
+        $contact->name = $request->input('name');
+        $contact->company_name = $request->input('company_name');
+        $contact->email = $request->input('email');
+        $contact->subject = $request->input('subject');
+        $contact->message = $request->input('message');
+
+        //TODO - sendemail
+
+        return $contact->save();
     }
 }
