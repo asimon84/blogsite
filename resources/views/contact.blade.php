9861999ec262bf4e58db0a3671f548aa69fbd0c4
asimon84 <ajsimon4@yahoo.com>
2025-10-29 20:25:06 -0700

    contact page map update

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -35,16 +35,14 @@
             </a>
             <div class="row">
                 <div class="col-12">
-                    <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6511980.545439102!2d-124.59221413351302!3d37.160355323932215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb9fe5f285e3d%3A0x8b5109a227086f55!2sCalifornia%2C%20USA!5e0!3m2!1sen!2sbd!4v1739717831300!5m2!1sen!2sbd"
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206544.9526618687!2d-115.22223774511413!3d36.013824496783386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c8c9ae1f5f4291%3A0xef1b2a38ca2061b5!2sHenderson%2C%20NV!5e0!3m2!1sen!2sus!4v1761794610497!5m2!1sen!2sus"
                             width="600"
                             height="800"
-                            style="border: 0"
+                            style="border:0;"
                             allowfullscreen=""
                             loading="lazy"
                             referrerpolicy="no-referrer-when-downgrade"
-                            class="d-block w-100"
-                    ></iframe>
+                            class="d-block w-100"></iframe>
                 </div>
             </div>
         </div>
