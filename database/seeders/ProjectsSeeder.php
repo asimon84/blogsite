a905b5797baf80e78d3f0f7f6e50578ff1ebee1b
asimon84 <ajsimon4@yahoo.com>
2026-01-12 20:30:21 -0800

    project seeder updates

--- database/seeders/ProjectsSeeder.php
+++ database/seeders/ProjectsSeeder.php
@@ -57,7 +57,7 @@ class ProjectsSeeder extends Seeder
             'repo' => 'https://github.com/asimon84/laravel-ai',
             'image' => 'storage/images/project/laravel-ai.png',
             'brief' => 'Laravel based AI/ML test project',
-            'description' => '',
+            'description' => 'Work in progress! Implementing RAG architecture, LLM libraries, and AI/ML coding concepts to produce basic automation examples.',
         ]);
     }
 }
