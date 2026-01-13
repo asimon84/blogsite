c5be3f8bbcf4eb493707e324f0159d7dcf39fc3b
asimon84 <ajsimon4@yahoo.com>
2026-01-12 21:11:44 -0800

    blog additions

---
+++ database/seeders/BlogsSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Blog;
+use Illuminate\Database\Seeder;
+
+class BlogSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Blog::factory()->create([
+            'name' => 'First Blog',
+            'content' => 'Some blog text.',
+        ]);
+    }
+}
