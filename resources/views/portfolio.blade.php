5bdce743a1bab08db242a9d810c6376d233e5484
asimon84 <ajsimon4@yahoo.com>
2026-01-22 13:32:53 -0800

    portfolio page code clean up

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -5,16 +5,11 @@
 @section('content')
 
     <!-- Hero section Start -->
-    <section
-            class="quanto-hero-section section-padding-bottom overflow-hidden"
-    >
+    <section class="quanto-hero-section section-padding-bottom overflow-hidden">
         <div class="container custom-container">
             <div class="row g-4">
                 <div class="col-lg-12 col-xxl-11">
-                    <div
-                            class="quanto-hero-common__content move-anim"
-                            data-delay="0.45"
-                    >
+                    <div class="quanto-hero-common__content move-anim" data-delay="0.45">
                         <h1 class="title">
                             Sample Projects
                         </h1>
@@ -26,9 +21,7 @@
     <!-- Hero section End -->
 
     <!-- Project section Start -->
-    <section
-            class="quanto-project-section bg-color-white section-padding-bottom"
-    >
+    <section class="quanto-project-section bg-color-white section-padding-bottom">
         <div class="container custom-container">
             <div class="row quanto_screenfix_right">
                 <div class="col-12 pe-0">
@@ -39,11 +32,7 @@
                                     <div class="quanto-project-box overflow-hidden">
                                         <a href="{{ route('project', ['id' => $project->id]) }}">
                                             <div class="quanto-project-thumb overflow-hidden">
-                                                <img
-                                                        src="{{ asset($project->image) }}"
-                                                        alt="project-thumb"
-                                                        class="w-100 img_reveal"
-                                                />
+                                                <img src="{{ asset($project->image) }}" alt="project-thumb" class="w-100 img_reveal"/>
                                             </div>
                                         </a>
                                         <div class="quanto-project-content">
@@ -60,9 +49,7 @@
                             @endforeach
                         </div>
                     </div>
-                    <div
-                            class="quanto-project__slider-navigation row-padding-top d-flex justify-content-center align-items-center gap-3"
-                    >
+                    <div class="quanto-project__slider-navigation row-padding-top d-flex justify-content-center align-items-center gap-3">
                         <div class="prev-btn">
                             <i class="fa-solid fa-arrow-left"></i>
                         </div>
